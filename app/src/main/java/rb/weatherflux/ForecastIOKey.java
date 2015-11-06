cab30911c57453accb4f07b3318a86a6840150f3
roib <roib@microsoft.com>
2015-11-06 12:37:58 +0200

    Renamed ForcastIO.java
    Due to collision with the library.

--- app/src/main/java/rb/weatherflux/ForecastIOKey.java
+++ app/src/main/java/rb/weatherflux/ForecastIOKey.java
@@ -3,6 +3,6 @@ package rb.weatherflux;
 /**
  * Holds the API key for forecastIO. DON'T publish your key!.
  */
-public class ForecastIO {
+public class ForecastIOKey {
     public static final String API_KEY = "";
 }
